1eda57906ef5f34e7f9a9f8f23110ad4eae3e093
vincenzosantopietro <vincenzo.santopietro@studenti.uniparthenope.it>
2018-03-01 14:36:15 +0100

    Reformatted code + moved relu after pooling

--- lenet5.py
+++ lenet5.py
@@ -1,61 +1,62 @@
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 from sklearn.utils import shuffle
-from tqdm import tqdm,trange
+from tqdm import tqdm, trange
+
 
 class Lenet5():
 
-    def __init__(self,train_data,train_labels,test_data,test_labels,validation_data=None,validation_labels=None,mean=0,stddev=0.3,learning_rate=0.001):
+    def __init__(self, train_data, train_labels, test_data, test_labels, validation_data=None, validation_labels=None,
+                 mean=0, stddev=0.3, learning_rate=0.001):
 
         self.train_data = train_data
         self.train_labels = train_labels
-        assert(len(self.train_labels)==len(self.train_data))
+        assert (len(self.train_labels) == len(self.train_data))
         assert (self.train_data[0].shape[0] == 32 and self.train_data[0].shape[1] == 32)
 
-        self.validation_data=validation_data
-        self.validation_labels=validation_labels
+        self.validation_data = validation_data
+        self.validation_labels = validation_labels
         assert (len(self.validation_labels) == len(self.validation_data))
         assert (self.validation_data[0].shape[0] == 32 and self.validation_data[0].shape[1] == 32)
 
-        self.test_data=test_data
-        self.test_labels=test_labels
+        self.test_data = test_data
+        self.test_labels = test_labels
         assert (len(self.test_data) == len(self.test_labels))
         assert (self.test_data[0].shape[0] == 32 and self.test_data[0].shape[1] == 32)
 
         self.num_outputs = 10
 
         self.X = tf.placeholder(tf.float32, shape=(None, 32, 32, 1))
-        self.y = tf.placeholder(tf.int32, shape=(None,self.num_outputs))
+        self.y = tf.placeholder(tf.int32, shape=(None, self.num_outputs))
 
         self.mu = mean
         self.sigma = stddev
 
         # Layer 1: Input 32x32x1, Output 28x28x6
-        self.conv1_kernels = tf.Variable(tf.truncated_normal(shape=[5,5,1,6],mean=self.mu,stddev=self.sigma))
+        self.conv1_kernels = tf.Variable(tf.truncated_normal(shape=[5, 5, 1, 6], mean=self.mu, stddev=self.sigma))
         self.conv1_biases = tf.Variable(tf.zeros(6))
-        self.conv1 = tf.nn.conv2d(self.X,self.conv1_kernels,[1,1,1,1],padding='VALID') + self.conv1_biases
-        # Activation
-        self.conv1 = tf.nn.relu(self.conv1)
+        self.conv1 = tf.nn.conv2d(self.X, self.conv1_kernels, [1, 1, 1, 1], padding='VALID') + self.conv1_biases
         # Pooling -> from 28x28 to 14x14
-        self.pool1 = tf.nn.max_pool(self.conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
-
+        self.pool1 = tf.nn.max_pool(self.conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Activation
+        self.conv1 = tf.nn.relu(self.pool1)
 
         # Layer 2: Input 14x14x6, Output 10x10x16
-        self.conv2_kernels = tf.Variable(tf.truncated_normal(shape=[5,5,6,16],mean=self.mu,stddev=self.sigma))
+        self.conv2_kernels = tf.Variable(tf.truncated_normal(shape=[5, 5, 6, 16], mean=self.mu, stddev=self.sigma))
         self.conv2_biases = tf.Variable(tf.zeros(16))
-        self.conv2 = tf.nn.conv2d(self.pool1,self.conv2_kernels,[1,1,1,1],padding='VALID') + self.conv2_biases
-        # Activation 2
-        self.conv2 = tf.nn.relu(self.conv2)
+        self.conv2 = tf.nn.conv2d(self.conv1, self.conv2_kernels, [1, 1, 1, 1], padding='VALID') + self.conv2_biases
         # Pooling -> from 10x10x16 to 5x5x16
-        self.pool2 = tf.nn.max_pool(self.conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='VALID')
+        self.pool2 = tf.nn.max_pool(self.conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Activation 2
+        self.conv2 = tf.nn.relu(self.pool2)
 
         # Flatten -> from 5x5x16 to 400x1
-        self.flattened = flatten(self.pool2)
+        self.flattened = flatten(self.conv2)
 
         # Fully Connected Layer n.1
-        self.fcl1_weights = tf.Variable(tf.truncated_normal(shape=[400,120],mean=self.mu,stddev=self.sigma))
+        self.fcl1_weights = tf.Variable(tf.truncated_normal(shape=[400, 120], mean=self.mu, stddev=self.sigma))
         self.fcl1_biases = tf.Variable(tf.zeros(120))
-        self.fcl1 = tf.matmul(self.flattened,self.fcl1_weights) + self.fcl1_biases
+        self.fcl1 = tf.matmul(self.flattened, self.fcl1_weights) + self.fcl1_biases
         # Activation 3
         self.fcl1 = tf.nn.relu(self.fcl1)
 
@@ -67,12 +68,12 @@ class Lenet5():
         self.fcl2 = tf.nn.relu(self.fcl2)
 
         # Fully Connected Layer n.3
-        self.fcl3_weights = tf.Variable(tf.truncated_normal(shape=[84,10],mean=self.mu,stddev=self.sigma))
+        self.fcl3_weights = tf.Variable(tf.truncated_normal(shape=[84, 10], mean=self.mu, stddev=self.sigma))
         self.fcl3_biases = tf.Variable(tf.zeros(10))
         self.logits = tf.matmul(self.fcl2, self.fcl3_weights) + self.fcl3_biases
 
         # Loss and metrics
-        self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits,labels=self.y)
+        self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.y)
         self.loss_op = tf.reduce_mean(self.cross_entropy)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         self.training_step = self.optimizer.minimize(self.loss_op)
@@ -81,9 +82,7 @@ class Lenet5():
         self.accuracy_operation = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
         self.saver = tf.train.Saver()
 
-
-
-    def train(self,epochs,batch_size):
+    def train(self, epochs, batch_size):
         assert (epochs > 0 and batch_size > 0)
 
         num_examples = len(self.train_data)
@@ -94,22 +93,24 @@ class Lenet5():
             session.run(tf.global_variables_initializer())
             total_steps = trange(epochs)
             for epoch in total_steps:
-                self.train_data,self.train_labels = shuffle(self.train_data, self.train_labels)
-                for offset in xrange(0,num_examples, batch_size):
+                self.train_data, self.train_labels = shuffle(self.train_data, self.train_labels)
+                for offset in xrange(0, num_examples, batch_size):
                     end = offset + batch_size
-                    X_batch,y_batch = self.train_data[offset:end], self.train_labels[offset:end]
+                    X_batch, y_batch = self.train_data[offset:end], self.train_labels[offset:end]
 
-                    _,acc,cross = session.run([self.training_step,self.accuracy_operation,self.cross_entropy], feed_dict={self.X: X_batch, self.y: y_batch})
+                    _, acc, cross = session.run([self.training_step, self.accuracy_operation, self.cross_entropy],
+                                                feed_dict={self.X: X_batch, self.y: y_batch})
 
                 if self.validation_data != None:
                     validation_accuracy = self.evaluate(self.validation_data, self.validation_labels, batch_size)
-                    #print("Epoch {} - validation accuracy {:.3f} ".format(epoch+1,validation_accuracy))
-                    total_steps.set_description("Epoch {} - validation accuracy {:.3f} ".format(epoch+1,validation_accuracy))
+                    # print("Epoch {} - validation accuracy {:.3f} ".format(epoch+1,validation_accuracy))
+                    total_steps.set_description(
+                        "Epoch {} - validation accuracy {:.3f} ".format(epoch + 1, validation_accuracy))
 
-            test_accuracy = self.evaluate(self.test_data,self.test_labels,batch_size=batch_size)
+            test_accuracy = self.evaluate(self.test_data, self.test_labels, batch_size=batch_size)
             return test_accuracy
 
-    def evaluate(self,X_data, y_data,batch_size):
+    def evaluate(self, X_data, y_data, batch_size):
         num_examples = len(X_data)
         total_accuracy = 0
         sess = tf.get_default_session()
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 from lenet5 import Lenet5
 
-mnist = input_data.read_data_sets("MNIST_data/",reshape=False,one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", reshape=False, one_hot=True)
 
-X_train,y_train = mnist.train.images,mnist.train.labels
-X_validation,y_validation = mnist.validation.images,mnist.validation.labels
-X_test,y_test = mnist.test.images,mnist.test.labels
+X_train, y_train = mnist.train.images, mnist.train.labels
+X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+X_test, y_test = mnist.test.images, mnist.test.labels
 
 print("Input shape: {}".format(X_train[0].shape))
 print("Training Set:   {} samples".format(len(X_train)))
@@ -14,12 +14,12 @@ print("Validation Set: {} samples".format(len(X_validation)))
 print("Test Set:       {} samples".format(len(X_test)))
 
 # 0-Padding for LeNet-5's input size
-X_train = np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
-X_validation = np.pad(X_validation, ((0,0),(2,2),(2,2),(0,0)), 'constant')
-X_test = np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+X_train = np.pad(X_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_validation = np.pad(X_validation, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
+X_test = np.pad(X_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
 
 print("New Input shape: {}".format(X_train[0].shape))
 
-lenet_network = Lenet5(X_train,y_train,X_test,y_test,X_validation,y_validation)
-accuracy = lenet_network.train(epochs=10,batch_size=100)
+lenet_network = Lenet5(X_train, y_train, X_test, y_test, X_validation, y_validation)
+accuracy = lenet_network.train(epochs=1, batch_size=100)
 print("Accuracy on test set: {:.3f}".format(accuracy))
